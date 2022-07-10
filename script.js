a3a6404eff070209ec52c050d2bf681333b6b831
Elizabeth G <elizabeth.g@keyvalue.systems>
2022-07-10 22:17:38 +0530

    formatted file

--- script.js
+++ script.js
@@ -1,61 +1,53 @@
-function change()
-        {
-            document.getElementById("btn").style.backgroundColor = "grey";
-            document.getElementById("btn").style.color = 'white';
-            
-            const isValidEmail=validateEmail();
-            if(!isValidEmail){
-                document.getElementById("btn").disabled = false;
-                alert("You have entered an invalid email address!")
-                return
-            }
-            document.getElementById("btn").disabled = true;
+function change() {
+  document.getElementById("btn").style.backgroundColor = "grey";
+  document.getElementById("btn").style.color = "white";
 
-        }
+  const isValidEmail = validateEmail();
+  if (!isValidEmail) {
+    document.getElementById("btn").disabled = false;
+    alert("You have entered an invalid email address!");
+    return;
+  }
+  document.getElementById("btn").disabled = true;
+}
 
-        function validateEmail() 
-        {
-            var email=document.getElementById("email").value;
-            console.log(email)
+function validateEmail() {
+  var email = document.getElementById("email").value;
+  console.log(email);
 
-            if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))
-            {
-                return (true)
-            }     
-            return (false)
-        }
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+    return true;
+  }
+  return false;
+}
 
-        const form = document.getElementById('contact-form');
+const form = document.getElementById("contact-form");
 
-        form.addEventListener('submit', callbackFunction);
-                            
-        function callbackFunction(event) {
-            event.preventDefault();
-            const myFormData = new FormData(event.target);
+form.addEventListener("submit", callbackFunction);
 
-            const formDataObj = {};
-            myFormData.forEach((value, key) => (formDataObj[key] = value));
-            console.log(formDataObj);
-        };
+function callbackFunction(event) {
+  event.preventDefault();
+  const myFormData = new FormData(event.target);
 
-        const debounce = (fn,delay)=>{
-            let timer;            
-            return function(...args){
-                if(timer){
-                    clearTimeout(timer);
-                }
-                timer=setTimeout(()=>{
-                    fn(...args);
+  const formDataObj = {};
+  myFormData.forEach((value, key) => (formDataObj[key] = value));
+  console.log(formDataObj);
+}
 
-                },delay);
-            };
-            
-        };
-        document.getElementById("btn").addEventListener("click",debounce(e =>{
-            console.log("clicked");
-        },2000))
-
-        
-      
-
-       
+const debounce = (fn, delay) => {
+  let timer;
+  return function (...args) {
+    if (timer) {
+      clearTimeout(timer);
+    }
+    timer = setTimeout(() => {
+      fn(...args);
+    }, delay);
+  };
+};
+document.getElementById("btn").addEventListener(
+  "click",
+  debounce((e) => {
+    console.log("clicked");
+  }, 2000)
+);
